e7a49cd8c3cda474349d1a0d4be189f191a6729d
Quick-Lightning <93387928+Quick-Lightning@users.noreply.github.com>
2022-10-23 22:51:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -15,8 +15,18 @@ async def on_ready():
 async def on_message(message):
     if message.author != client.user and message.attachments:
         for file in message.attachments:
-            for ext in ['.jpg','.png','.jpeg']:
+            for ext in ['.jpg', '.png', '.jpeg']:
                 if file.filename.endswith(ext):
                     await message.channel.send("That message contains an image!")
+                    time.sleep(random.randrange(1, 3))
+                    channel1 = client.get_channel(1032676870912753676)
+                    channel2 = client.get_channel(1033322865694625905)
+                    embed = discord.Embed(title="Would you like to publish this wallpaper?", description="Publishing will send post this wallpaper in other union servers' wallpaper channels. If the image is a wallpaper it is recommended to publish.", color=discord.Colour.blue())
+                    embed.set_image(url=message.attachments[0].url)
+                    embed.set_footer(text="Note that publishing non wallpaper images or low quality shitpost wallpapers can lead to an infraction.")
+                    await channel1.send(embed=embed)
+                    await channel2.send(embed=embed)
+
 
 client.run(BOT_TOKEN)
+
